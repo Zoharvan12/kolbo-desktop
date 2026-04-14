0a5a89fec04ee9f0aa92471b408d7ec955f72bb1
Zoharvan12 <91822245+Zoharvan12@users.noreply.github.com>
2026-04-14 18:40:01 +0300

    feat: inject whitelabel vars into build-env.js

--- build-env.js
+++ build-env.js
@@ -4,16 +4,23 @@ const path = require('path');
 
 // Get environment from command line argument or environment variable
 const env = process.env.KOLBO_ENV || 'development';
+const whitelabel = process.env.KOLBO_WHITELABEL || '';
+const whitelabelAppUrl = process.env.KOLBO_WHITELABEL_APP_URL || '';
+const whitelabelApiUrl = process.env.KOLBO_WHITELABEL_API_URL || '';
 
 console.log(`[Build] Writing environment: ${env}`);
+if (whitelabel) console.log(`[Build] Whitelabel: ${whitelabel} → ${whitelabelAppUrl}`);
 
 // Create environment config file
 const envConfig = `// Auto-generated during build - DO NOT EDIT MANUALLY
 // This file is created by build-env.js
 
 window.KOLBO_BUILD_ENV = '${env}';
+window.KOLBO_WHITELABEL = '${whitelabel}';
+window.KOLBO_WHITELABEL_APP_URL = '${whitelabelAppUrl}';
+window.KOLBO_WHITELABEL_API_URL = '${whitelabelApiUrl}';
 
-console.log('[Build Environment] Loaded:', window.KOLBO_BUILD_ENV);
+console.log('[Build Environment] Loaded:', window.KOLBO_BUILD_ENV, window.KOLBO_WHITELABEL || '(Kolbo)');
 `;
 
 // Write to src/renderer directory
